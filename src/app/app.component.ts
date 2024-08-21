aca8d1198833704b74fb4293dcfec9d83f099a15
Pablo Pita <ppitacaneiro@gmail.com>
2024-08-21 20:28:31 +0200

    prueba de concepto de extraccion de colores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FlowbiteService } from './services/flowbite.service';
+import { extractColors } from 'extract-colors';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,19 @@ import { FlowbiteService } from './services/flowbite.service';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  title = 'logotoui';
+  
+  title:string = 'logotoui';
+  src:string = 'google-logo.png';
 
   constructor(private flowbiteService: FlowbiteService) {}
 
   ngOnInit() {
     this.flowbiteService.loadFlowbite(flowbite => {
       console.log('Flowbite loaded', flowbite);
+    });
+
+    extractColors(this.src).then(colors => {
+      console.log(colors);
     });
   }
 }
